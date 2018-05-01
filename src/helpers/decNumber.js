05c8dbe90346c61bd6693d8f9751c55ae4073b86
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-05-02 00:21:45 +0300

    decrement function changed

--- src/helpers/decNumber.js
+++ src/helpers/decNumber.js
@@ -1,5 +1,7 @@
 import * as R from '@paqmind/ramda'
 
-const decNumber = (x) => R.is(Number, x) ? (x >= 2 ? x - 1 : null) : null
+const decNumber = (x) => R.is (Object, x)
+  ? (x.countdown >= 2 ? {...x, countdown: x.countdown - 1} : x)
+  : null
 
 export default decNumber
