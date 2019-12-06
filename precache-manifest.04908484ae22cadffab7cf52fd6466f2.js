89874fea1cea7a0d8d03bb82a8c04778879cf5c0
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2019-12-06 17:31:31 +0300

    Updates

---
+++ precache-manifest.04908484ae22cadffab7cf52fd6466f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71fecc6ca797cb4fd5811e825eb1a9b",
+    "url": "/switch-game/index.html"
+  },
+  {
+    "revision": "6ed5444f3eeba6aaf828",
+    "url": "/switch-game/static/css/main.b7378bf6.chunk.css"
+  },
+  {
+    "revision": "f684ea000889f2aac579",
+    "url": "/switch-game/static/js/2.95ede276.chunk.js"
+  },
+  {
+    "revision": "68194534981a47be39e932fc2acb9406",
+    "url": "/switch-game/static/js/2.95ede276.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ed5444f3eeba6aaf828",
+    "url": "/switch-game/static/js/main.de964e18.chunk.js"
+  },
+  {
+    "revision": "b30a7012a11ab3cb5a4c",
+    "url": "/switch-game/static/js/runtime-main.0f7d6049.js"
+  }
+]);
