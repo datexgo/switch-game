696fa6c4de50b0e877808789572c99c9702b48e2
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2019-12-13 17:43:32 +0300

    add round waves animation

--- src/components/GameProgress.js
+++ src/components/GameProgress.js
@@ -4,6 +4,24 @@ import PropTypes from 'prop-types'
 const GameProgress = props => {
   const { state } = props
 
+  // const fr = [
+  //   { width: '30px', height: '30px', opacity: '1' },
+  //   { width: '120px', height: '120px', opacity: '0' }
+  // ]
+
+  // const frTiming = {
+  //   duration: 1500,
+  //   iterations: Infinity
+  // }
+
+  // const an = () => {
+  //   const el = document.getElementById('anim')
+  //   el.animate(
+  //     fr,
+  //     frTiming
+  //   )
+  // }
+
   return <div className="game-progress">
     <div className="circle">
       <div>
