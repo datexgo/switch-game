caf5516d056c7a868de4942ae892519764a95ddd
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2019-12-15 14:02:47 +0300

    init vk mini app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Game from './components/Game'
+import connect from '@vkontakte/vk-connect'
 import '@vkontakte/vkui/dist/vkui.css'
 import './styles/main.scss'
 
-document.cookie = 'SameSite=Strict'
+connect.send("VKWebAppInit", {})
 
 ReactDOM.render(
   <Game/>,
