8a318e45de7e9562fb0d7874f5013eab5e03a96c
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2019-12-19 00:21:30 +0300

    reduce circle animation opacity

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,8 +3,8 @@ export const gridPadding = 20
 
 export const circleAnimationFrame = [
   { width: '60px', height: '60px', opacity: '0' },
-  { width: '90px', height: '90px', opacity: '1', offset: 0.3 },
-  { width: '120px', height: '120px', opacity: '0' }
+  { width: '90px', height: '90px', opacity: '0.1', offset: 0.3 },
+  { width: '110px', height: '110px', opacity: '0' }
 ]
 
 export const circleAnimationTiming = {
